0d0441b24f909726dcabe3f43c6b8721f2783ac1
KovuTheHusky <kovuthehusky@gmail.com>
2020-04-24 18:56:31 -0400

    Optimize images

--- optimize.sh
+++ optimize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -type f -name "*.png" -exec pngquant --force --skip-if-larger --output {} --speed 1 --strip {} \;
+find . -type f -name "*.png" -exec pngquant --force --skip-if-larger --output {} --speed 1 --strip --verbose {} \;
 find . -type f -name "*.svg" -exec svgcleaner {} {} \;
 find . -type f -name "*.svg" -exec svgo --multipass {} \;
