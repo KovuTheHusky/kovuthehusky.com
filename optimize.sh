8e6f7065c20d47b7cbcd000875a43314d4927556
KovuTheHusky <kovuthehusky@gmail.com>
2020-12-07 11:54:57 -0500

    Update optimize script

--- optimize.sh
+++ optimize.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-find . -type f -name "*.png" -exec pngquant --force --skip-if-larger --output {} --speed 1 --strip {} \;
-find . -type f -name "*.svg" -exec svgcleaner {} {} \;
+for FILE in $(find . \( -path ./_site -o -path ./node_modules \) -prune -false -o -iname "*.svg"); do 
+    svgcleaner "$FILE" "$FILE"
+done
 svgo -f . -r -q --multipass
+
+for FILE in $(find . \( -path ./_site -o -path ./node_modules \) -prune -false -o -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" -o -iname "*.tif" -o -iname "*.tiff"); do 
+    cwebp "$FILE" -o "${FILE%.*}".webp;
+done
+
+for FILE in $(find . \( -path ./_site -o -path ./node_modules \) -prune -false -o -iname "*.png"); do 
+    pngquant --force --skip-if-larger --output "$FILE" --speed 1 --strip "$FILE"
+done
