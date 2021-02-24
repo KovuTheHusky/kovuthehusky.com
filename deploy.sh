65e630856a44f189f19e4d64cc9cd4eb37ad2869
KovuTheHusky <kovuthehusky@gmail.com>
2021-02-24 18:20:31 -0500

    Fix some GitHub Actions stuff

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-    exit 0
-fi
-
 set -e
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-echo "Updating pokédex..."
-ruby pokemongo.rb
-echo "Updating projects..."
-curl -X GET "https://status.kovuthehusky.com/projects.json" > _data/projects.json
-
-rm -rf _site
-mkdir _site
-git clone https://${GITHUB_TOKEN}@github.com/KovuTheHusky/kovuthehusky.com.git --branch gh-pages _site
-bundle exec jekyll build
 cd _site
-if [ -n "$(git status --porcelain)" ]; then
-    git add --all
-    git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-    git push --force origin gh-pages
-fi
+
+git init
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -m "${GITHUB_RUN_NUMBER}"
+git push --force https://KovuTheHusky:${{secrets.JEKYLL_PAT}}@github.com/${GITHUB_REPOSITORY}.git master:gh-pages
